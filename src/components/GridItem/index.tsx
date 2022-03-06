66cbc9561da475ec76dc3c6126cd12f0390c84b3
Natan <natan.carvalho123@hotmail.com>
2022-03-06 12:44:46 -0300

    Creating the GridItem component and styling

--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -1,8 +1,27 @@
 import { GridItemProps } from "./type";
-
+import styles from "./GridItem.module.css";
+import upImage from "../../assets/up.png";
+import downImage from "../../assets/down.png";
 
 export function GridItem({ item }: GridItemProps) {
   return (
-    <></>
+    <div
+      className={styles.main}
+      style={{ backgroundColor: item.color }}
+    >
+      <div className={styles.gridIcon}>
+        <img
+          src={item.icon === "up" ? upImage : downImage}
+          alt=""
+          width={30}
+        />
+      </div>
+      <div className={styles.gridTitle}>{item.title}</div>
+      <div className={styles.gridInfo}>
+        <>
+          IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
+        </>
+      </div>
+    </div>
   )
 }
